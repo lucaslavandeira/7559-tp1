a60097e2e24dbd3839317af4e7a64bdcf6c57558
Lucas Lavandeira <lucaslavandeira@gmail.com>
2019-09-15 22:23:59 -0300

    Inventario de sells center

--- src/centers/sells_center.cpp
+++ src/centers/sells_center.cpp
@@ -1,30 +1,34 @@
 #include "sells_center.h"
 #include "../routes/route.h"
-#include "../ipc/ExitFlag.h"
 #include <iostream>
 #include <zconf.h>
 
-SellsCenter::SellsCenter() {
-}
+SellsCenter::SellsCenter() = default;
 
-void SellsCenter::associate_route(DistributionRoute& route) {
-  this->route = route;
+void SellsCenter::associate_route(DistributionRoute &distributionRoute) {
+    this->route = distributionRoute;
 }
 
 FlowerPacket SellsCenter::receive() {
-  return this->route.receive();
+    FlowerPacket packet = this->route.receive();
+    if (!flowers.count(packet.type)) {
+        flowers[packet.type] = 0;
+    }
+    flowers[packet.type] = flowers[packet.type] + packet.ammount;
+    return packet;
 }
 
 void SellsCenter::work() {
-  while (true) {
-    try {
-        FlowerPacket packet = this->receive();
-        std::cout << "Hola del punto de venta! Recibí " << packet.ammount << " de " << packet.type << " desde el centro!!!" << std::endl;
-    } catch(int e) {
-        break;
+    while (true) {
+        try {
+            FlowerPacket packet = this->receive();
+            std::cout << "Hola del punto de venta! Recibí " << packet.ammount << " de " << packet.type
+                      << " desde el centro!!!" << std::endl;
+        } catch (int e) {
+            break;
+        }
+        sleep(1);
     }
-    sleep(1);
-  }
 
-  std::cout << "Sells center fue cerrado!!" << std::endl;
+    std::cout << "Sells center fue cerrado!! Tenía " << flowers["tulip"] << " tulips" << std::endl;
 }
--- src/centers/sells_center.h
+++ src/centers/sells_center.h
@@ -1,16 +1,21 @@
 #ifndef SELLS_CENTER_H
 #define SELLS_CENTER_H
 
+#include <unordered_map>
+#include <string>
 #include "../routes/distribution_route.h"
 
 class SellsCenter {
+    std::unordered_map<std::string, int> flowers;
 public:
   DistributionRoute route;
 
   SellsCenter();
-  void associate_route(DistributionRoute& route);
-  FlowerPacket receive();
+  void associate_route(DistributionRoute& distributionRoute);
   void work();
+
+private:
+    FlowerPacket receive();
 };
 
 #endif
